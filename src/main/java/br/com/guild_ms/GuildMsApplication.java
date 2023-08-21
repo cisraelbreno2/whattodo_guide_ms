68058d629dab10e06ec33ca88223c3581089dcbd
Israel Breno Costa <cisraelbreno@gmail.com>
2023-08-21 10:58:53 -0300

    feat: :sparkles: Initial commit

---
+++ src/main/java/br/com/guild_ms/GuildMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.guild_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuildMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuildMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/guild_ms/GuildMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guild_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuildMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
